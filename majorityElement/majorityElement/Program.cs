aa44dba9a0c5f824c320e93d9f73085cd07a0b67
john almozlino <johnalmozlino@gmail.com]>
2021-05-20 19:39:41 +0300

    added the C# file

---
+++ majorityElement/majorityElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace majorityElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
