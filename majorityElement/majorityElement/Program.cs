932593d1ecdf623f5efefd1a675e596e1704a87d
john almozlino <johnalmozlino@gmail.com]>
2021-05-23 15:15:30 +0300

    added the majorityElement function

--- majorityElement/majorityElement/Program.cs
+++ majorityElement/majorityElement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace majorityElement
 {
@@ -6,7 +7,34 @@ namespace majorityElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numList = new List<int>() { 1, 1, 1, 1, 1, 1, 4, 5, 6, 7, 8 };
+           
+            Console.WriteLine(majorityElement(numList));
+
+
+        }
+
+        private static int majorityElement(List<int> numList)
+        {
+            int count = 1;
+            int majorNum = -1;
+            for (int i = 0; i < numList.Count; i++)
+            {
+                if(majorNum != numList[i])
+                {
+                    count--;
+                }
+                else
+                {
+                    count++;
+                }
+                if (count ==0)
+                {
+                    majorNum = numList[i];
+                    
+                }
+            }
+            return majorNum;
         }
     }
 }
